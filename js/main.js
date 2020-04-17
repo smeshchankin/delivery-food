97ee96da5e814e5df677e5ed442203278a0dfd56
Sergey Meshchankin <smeshchankin@gmail.com>
2020-04-18 00:18:49 +0300

    [DF-008] Modal dialog: Populate product list

--- js/main.js
+++ js/main.js
@@ -1,26 +1,47 @@
 (function() {
-    let data = [
+    let restaurants = [
         {img: '01.jpg', title: 'Pizza Plus Plus', tag: '50 min', rating: 4.5, price: 'From $9.99', category: 'Pizza'},
         {img: '02.jpg', title: 'Pizza Minus Minus', tag: '150 min', rating: 4.1, price: 'From $4.99', category: 'Pizza'},
         {img: '03.jpg', title: 'Pizza Plus Plus', tag: '50 min', rating: 4.5, price: 'From $9.99', category: 'Pizza'},
         {img: '04.jpg', title: 'Pizza Minus Minus', tag: '150 min', rating: 4.1, price: 'From $4.99', category: 'Pizza'},
         {img: '05.jpg', title: 'Pizza Plus Plus', tag: '50 min', rating: 4.5, price: 'From $9.99', category: 'Pizza'},
         {img: '06.jpg', title: 'Pizza Minus Minus', tag: '150 min', rating: 4.1, price: 'From $4.99', category: 'Pizza'}
-
+    ];
+    let cart = [
+        {name: 'God Pizza', price: '$43.00', count: 4},
+        {name: 'The best sushi', price: '$11.99', count: 3},
+        {name: 'All in one dishes', price: '$19.99', count: 2}
     ];
 
-    let cards = document.querySelector('#cards');
-    let card = cards.querySelector('.card');
+    populateData('#cards', '.card', restaurants);
+    populateData('#product-list', '.product-row', cart);
 
-    cards.removeChild(card);
 
-    for (let i = 0; i < data.length; i++) {
-        cards.appendChild(fillCard(card, data[i]));
+    const modal = document.querySelector('.modal');
+    const cartButton = document.querySelector('#cart');
+    const closeButton = document.querySelector('.close');
+    cartButton.addEventListener('click', toggleModal);
+    closeButton.addEventListener('click', toggleModal);
+
+    function toggleModal() {
+        modal.classList.toggle('is-open');
     }
 
-    function fillCard(template, data) {
-        let node = template.cloneNode(true);
-        node.innerHTML = node.innerHTML.replace('00.jpg', data.img);
+    function populateData(parentSelector, templateSelector, data) {
+        let parentNode = document.querySelector(parentSelector);
+        let templateNode = parentNode.querySelector(templateSelector);
+        parentNode.removeChild(templateNode);
+
+        for (let i = 0; i < data.length; i++) {
+            parentNode.appendChild(fillNode(templateNode, data[i]));
+        }
+    }
+
+    function fillNode(templateNode, data) {
+        let node = templateNode.cloneNode(true);
+        if (data.img) {
+            node.innerHTML = node.innerHTML.replace('00.jpg', data.img);
+        }
 
         Object.keys(data).forEach(function(key) {
             let value = data[key];
@@ -28,14 +49,5 @@
         });
 
         return node;
-    }
-
-    const modal = document.querySelector('.modal');
-    const cartButton = document.querySelector('#cart');
-    const closeButton = document.querySelector('.close');
-    cartButton.addEventListener('click', toggleModal);
-    closeButton.addEventListener('click', toggleModal);
-    function toggleModal() {
-        modal.classList.toggle('is-open');
     }
 }());
